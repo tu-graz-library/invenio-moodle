b5231d0f8c9beedaff6e8c3d6a3eba9e14b0e259
Christoph Ladurner <christoph.ladurner@tugraz.at>
2025-07-05 21:38:13 +0200

    job: add dry run option

--- invenio_moodle/jobs.py
+++ invenio_moodle/jobs.py
@@ -8,9 +8,21 @@
 """Jobs."""
 
 
-from invenio_jobs.jobs import JobType
+from invenio_jobs.jobs import JobType, PredefinedArgsSchema
+from invenio_jobs.models import Job
+from marshmallow.fields import String
 
 from .tasks import import_records
+
+
+class MoodlePredefinedArgsSchema(PredefinedArgsSchema):
+    """Moodle predefined args schema."""
+
+    dry_run = String(
+        metadata={
+            "description": "bool value to describe dry run",
+        },
+    )
 
 
 class ImportMoodleRecordsJob(JobType):
@@ -21,3 +33,16 @@ class ImportMoodleRecordsJob(JobType):
     description = "Import moodle records."
 
     task = import_records
+
+    arguments_schema = MoodlePredefinedArgsSchema
+
+    @classmethod
+    def build_task_arguments(
+        cls,
+        job_obj: Job,  # noqa: ARG003
+        dry_run: str | None = None,
+        **__: dict,
+    ) -> dict[str, bool]:
+        """Define extra arguments to be injected on task execution."""
+        parsed_dry_run = dry_run in ["true", "True", 1, "1", "t", "T"]
+        return {"dry_run": parsed_dry_run}
